57e52adf18fbc892a6b29ccf192858b3b94f323e
NHTHEBEST <noreply.nhthebest@gmail.com>
2018-09-06 18:20:21 +1200

    start deving

---
+++ API client/data io.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+
+namespace TPKC.API
+{
+    class DataIO
+    {
+        public SQLiteConnection DB;
+
+        public DataIO(string DatabasePath)
+        {
+            bool n = false;
+            if (!File.Exists(DatabasePath))
+            {
+                SQLiteConnection.CreateFile(DatabasePath);
+                n = true;
+            }
+
+            DB = new SQLiteConnection(@"Data Source=" + DatabasePath + ";Version=3;");
+            
+            if(n)
+            {
+                
+            }
+
+        }
+
+        public void RunSQL(string sql)
+        {
+            DB.Open();
+            new SQLiteCommand(sql, DB).ExecuteNonQuery();
+            DB.Close();
+        }
+    }
+}
