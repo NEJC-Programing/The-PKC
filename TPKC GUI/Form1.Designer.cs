4e346b764cc4da7c960d707fd377fa192456f9bd
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2018-09-09 17:46:32 +1200

    ????

--- TPKC GUI/Form1.Designer.cs
+++ TPKC GUI/Form1.Designer.cs
@@ -28,14 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -45,55 +40,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.splitContainer1.Location = new System.Drawing.Point(-5, -2);
             this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.fastColoredTextBox1);
             this.splitContainer1.Size = new System.Drawing.Size(795, 290);
             this.splitContainer1.SplitterDistance = 495;
             this.splitContainer1.TabIndex = 0;
             this.splitContainer1.SplitterMoving += new System.Windows.Forms.SplitterCancelEventHandler(this.splittermoveing);
-            // 
-            // fastColoredTextBox1
-            // 
-            this.fastColoredTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
-        '(',
-        ')',
-        '{',
-        '}',
-        '[',
-        ']',
-        '\"',
-        '\"',
-        '\'',
-        '\''};
-            this.fastColoredTextBox1.AutoIndentCharsPatterns = "";
-            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(179, 14);
-            this.fastColoredTextBox1.BackBrush = null;
-            this.fastColoredTextBox1.CharHeight = 14;
-            this.fastColoredTextBox1.CharWidth = 8;
-            this.fastColoredTextBox1.CommentPrefix = null;
-            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
-            this.fastColoredTextBox1.IsReplaceMode = false;
-            this.fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.HTML;
-            this.fastColoredTextBox1.LeftBracket = '<';
-            this.fastColoredTextBox1.LeftBracket2 = '(';
-            this.fastColoredTextBox1.Location = new System.Drawing.Point(3, 3);
-            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
-            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
-            this.fastColoredTextBox1.RightBracket = '>';
-            this.fastColoredTextBox1.RightBracket2 = ')';
-            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
-            this.fastColoredTextBox1.Size = new System.Drawing.Size(489, 287);
-            this.fastColoredTextBox1.TabIndex = 0;
-            this.fastColoredTextBox1.Text = "fastColoredTextBox1";
-            this.fastColoredTextBox1.Zoom = 100;
-            this.fastColoredTextBox1.TextChanged += new System.EventHandler<FastColoredTextBoxNS.TextChangedEventArgs>(this.TextChangedd);
             // 
             // Form1
             // 
@@ -103,10 +53,8 @@
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.splitContainer1.Panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -114,7 +62,6 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
     }
 }
 
--- TPKC GUI/Form1.cs
+++ TPKC GUI/Form1.cs
@@ -9,12 +9,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using CefSharp.WinForms;
 using CefSharp;
+using FastColoredTextBoxNS;
 
 namespace TPKC_GUI
 {
     public partial class Form1 : Form
     {
         ChromiumWebBrowser browser;
+        FastColoredTextBox fastColoredTextBox1;
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +26,13 @@ namespace TPKC_GUI
             browser.Height = splitContainer1.Panel2.Height;
             browser.Parent = splitContainer1.Panel2;
             browser.Show();
+            fastColoredTextBox1 = new FastColoredTextBox();
+            fastColoredTextBox1.Anchor = (AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left | AnchorStyles.Bottom);
+            fastColoredTextBox1.Width = splitContainer1.Panel1.Width;
+            fastColoredTextBox1.Height = splitContainer1.Panel1.Height;
+            fastColoredTextBox1.Parent = splitContainer1.Panel1;
+            fastColoredTextBox1.TextChanged += FastColoredTextBox1_TextChanged;
+            fastColoredTextBox1.Show();
         }
 
         private void splittermoveing(object sender, SplitterCancelEventArgs e)
@@ -31,7 +40,7 @@ namespace TPKC_GUI
             
         }
 
-        private void TextChangedd(object sender, FastColoredTextBoxNS.TextChangedEventArgs e)
+        private void FastColoredTextBox1_TextChanged(object sender, FastColoredTextBoxNS.TextChangedEventArgs e)
         {
             try
             {
